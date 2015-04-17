10134e99416c0655c5d56dfc43596b89f3f1842f
rgmz <rgmz@gmail.com>
2015-04-17 12:01:33 +0530

    Rename files and minor fix to run_analysis.R

--- Project/run_analysis.R
+++ Project/run_analysis.R
@@ -24,18 +24,6 @@ test_subject<-read.table(test_subject_file)
 features<-read.table(features_file)
 activity_label<-read.table(activity_label_file)
 
-#Check dimensions of data, set if(T) to run
-if(F) {
-  dim(X_train)
-  dim(X_test)
-  dim(Y_train)
-  dim(Y_test)
-  dim(train_subject)
-  dim(test_subject)
-  dim(features)
-  dim(activity_label)
-}
-
 #Apply Column Names
 
 names(X_train)<-features[,2]
@@ -50,20 +38,9 @@ names(activity_label)<-c("activity_no.","activity_label")
 test_df<-cbind(X_test,Y_test,test_subject)
 train_df<-cbind(X_train,Y_train,train_subject)
 
-#Check dimensions and names of data, set if(T) to run
-if(F) {
-  dim(test_df)
-  dim(train_df)
-  names(test_df)
-  names(train_df)
-}
-
 #Bind test data and train data
 
 data<-rbind(train_df,test_df)
-
-#Check dimensions of data, set if(T) to run
-if(F) {dim(data)}
 
 #Get Column names of features with mean() and std() in their names
 
@@ -80,21 +57,9 @@ extract_names<-c("activity_no.","subject_no.",required_features)
 
 data_extract<-data[,extract_names]
 
-#Check dimensions and names of data_extract, set if(T) to run
-if(F) {
-  dim(data_extract)
-  names(data_extract)
-}
-
 #Use descriptive activity names to name the activities in the data set
 
 data_extract<-merge(activity_label, data_extract, by.x = "activity_no.", by.y = "activity_no.",)
-
-#Check dimensions and names of data_extract, set if(T) to run
-if(F){
-  dim(data_extract)
-  names(data_extract)
-}
 
 #Split data by activity and further by subject
 data_extract_split<-split(data_extract,list(data_extract$activity_label,data_extract$subject_no.))
@@ -103,7 +68,7 @@ data_extract_split<-split(data_extract,list(data_extract$activity_label,data_ext
 data_extract_means<-sapply(
   data_extract_split,
   function(x) {
-    colMeans(x[,names(x)!=c("activity_no.", "activity_label", "subject_no.")])
+    colMeans(x[,!names(x) %in% c("activity_no.", "activity_label", "subject_no.")])
   }
   )
 
@@ -118,6 +83,9 @@ data_extract_means<-cbind(
   data_extract_means
   )
 colnames(data_extract_means)[c(1,2)]<-c("subject_no.","activity_label")
+
+#Tidy up data
+
 add_label<-function(x) {
   for(i in rownames(x)) {
     x[i,c(2,1)]<-tolower(strsplit(x = i, split = "\\.")[[1]])
@@ -126,11 +94,5 @@ add_label<-function(x) {
 }
 
 data_extract_means<-add_label(data_extract_means)
-
-if(F) {
-  head(data_extract_means[,1:5])
-  dim(data_extract_means)
-  class(data_extract_means)
-}
 
 write.table(file= output_means_extracted, x = data_extract_means, row.names = F)
